0cf91727de199b6912500517840f8f463e3f07f1
klyutko <klyutko.d@gmail.com>
2023-03-18 00:55:41 +0600

    remove contact

--- components/Services/Service.vue
+++ components/Services/Service.vue
@@ -1,18 +1,20 @@
 <template>
-    <article class="mb-10 bg-[#161513] p-10 rounded-lg" data-aos="fade-left">
-        <h1 class="text-4xl mb-5">{{ service.title }}</h1>
-        <p class="mb-10 text-gray-400">
-            {{ service.description }}
-        </p>
-        <UIServicesButton class="mb-10">HIRE ME</UIServicesButton>
-        <img :src="service.img" class="w-2/3 max-lg:w-full" alt="">
-    </article>
+  <article class="mb-10 bg-[#161513] p-10 rounded-lg" data-aos="fade-left">
+    <h1 class="text-4xl mb-5">{{ service.title }}</h1>
+    <p class="mb-10 text-gray-400">
+      {{ service.description }}
+    </p>
+    <a href="https://t.me/klyutko7p">
+      <UIServicesButton class="mb-10">HIRE ME</UIServicesButton>
+    </a>
+    <img :src="service.img" class="w-2/3 max-lg:w-full" alt="" />
+  </article>
 </template>
 
 <script setup lang="ts">
 defineProps({
-    service: { type: Object as PropType<Service>, required: true }
-})
+  service: { type: Object as PropType<Service>, required: true },
+});
 </script>
 
 <style scoped></style>
--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,37 +1,47 @@
 <template>
-    <footer class="mb-10">
-        <nav class="flex items-center justify-between border-t-2 border-gray-500 py-10 max-lg:flex-col">
-            <div>
-                <img src="~/assets/images/klyutko-logo.svg" @click="$router.push('/')" class="cursor-pointer w-full h-16"
-                    alt="">
-            </div>
-            <div class="max-lg:mt-5 max-md:flex max-md:flex-col text-center">
-                <NuxtLink active-class="gradient-text" to="/"
-                    class="font-['Bai_Jamjuree'] gradient-color cursor-pointer duration-300 hover:scale-110 font-medium mr-5 text-lg">
-                    Home
-                </NuxtLink>
-                <NuxtLink active-class="gradient-text" to="/services"
-                    class="font-['Bai_Jamjuree'] gradient-color cursor-pointer duration-300 hover:scale-110 font-medium mr-5 text-lg">
-                    Services
-                </NuxtLink>
-                <NuxtLink active-class="gradient-text" to="/projects"
-                    class="font-['Bai_Jamjuree'] gradient-color cursor-pointer duration-300 hover:scale-110 font-medium mr-5 text-lg">
-                    Projects
-                </NuxtLink>
-                <NuxtLink active-class="gradient-text" to="/contact"
-                    class="font-['Bai_Jamjuree'] gradient-color cursor-pointer duration-300 hover:scale-110 font-medium mr-5 text-lg">
-                    Contact
-                </NuxtLink>
-            </div>
-        </nav>
-        <div class="flex items-center justify-center">
-            <h1 class="uppercase mr-10">© developed by Daniel klyutko</h1>
-        </div>
-    </footer>
+  <footer class="mb-10">
+    <nav
+      class="flex items-center justify-between border-t-2 border-gray-500 py-10 max-lg:flex-col"
+    >
+      <div>
+        <img
+          src="~/assets/images/klyutko-logo.svg"
+          @click="$router.push('/')"
+          class="cursor-pointer w-full h-16"
+          alt=""
+        />
+      </div>
+      <div class="max-lg:mt-5 max-md:flex max-md:flex-col text-center">
+        <NuxtLink
+          active-class="gradient-text"
+          to="/"
+          class="font-['Bai_Jamjuree'] gradient-color cursor-pointer duration-300 hover:scale-110 font-medium mr-5 text-lg"
+        >
+          Home
+        </NuxtLink>
+        <NuxtLink
+          active-class="gradient-text"
+          to="/services"
+          class="font-['Bai_Jamjuree'] gradient-color cursor-pointer duration-300 hover:scale-110 font-medium mr-5 text-lg"
+        >
+          Services
+        </NuxtLink>
+        <NuxtLink
+          active-class="gradient-text"
+          to="/projects"
+          class="font-['Bai_Jamjuree'] gradient-color cursor-pointer duration-300 hover:scale-110 font-medium mr-5 text-lg"
+        >
+          Projects
+        </NuxtLink>
+      </div>
+    </nav>
+    <div class="flex items-center justify-center">
+      <h1 class="uppercase mr-10">© developed by Daniel klyutko</h1>
+    </div>
+  </footer>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
 <style scoped></style>
